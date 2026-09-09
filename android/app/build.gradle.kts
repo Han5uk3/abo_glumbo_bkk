116160467b17c5d2a408e6a7fa0d4aecd12396a8
Han5uk3 <adnanyousufpangat@gmail.com>
2026-09-09 18:36:24 +0530

    feat: update versioning in pubspec.yaml and adjust Android permissions in manifest

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
         
         manifestPlaceholders["GOOGLE_MAPS_API_KEY"] = googleMapsApiKey
 
